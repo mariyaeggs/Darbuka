d35b767b9b259c37ecec28d4c1d52f4ed8c1eced
mariyaeggs <mariyawilson@live.com>
2018-02-01 12:48:44 -0800

    Setup sound file assets.

---
+++ Darbuka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Darbuka
+//
+//  Created by Mariya Eggensperger  on 2/1/18.
+//  Copyright © 2018 Mariya Eggensperger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
